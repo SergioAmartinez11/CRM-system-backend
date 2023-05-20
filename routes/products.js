a262b4962337f3369fa5df24f336246bb2190cff
Sergio A. Martinez <71477549+SergioAmartinez11@users.noreply.github.com>
2023-05-19 19:26:25 -0700

    Terminado
    Se agrego todas las rutas y queries para validar las consultas y manejo de llamadas del front end.

--- routes/products.js
+++ routes/products.js
@@ -24,7 +24,7 @@ router.get('/', function (req, res, next) {
       return;
     } else {
       console.log(`Se obtuvieron ${results.rowCount} filas de la tabla`);
-      console.log(results.rows);
+      //console.log(results.rows);
     }
 
     data = JSON.stringify(results.rows);
@@ -34,13 +34,49 @@ router.get('/', function (req, res, next) {
   pool.end();
 });
 
+router.put('/update/:id', function (req, res, next) {
+  const pool = new Pool({
+    user: 'sergio-martinez',
+    host: 'localhost',
+    database: 'crm',
+    password: '',
+    port: 5432, // puerto predeterminado de PostgreSQL
+  });
+  const timeStamp = new Date();
+  const updatedData = req.body;
+  console.log(updatedData);
+  const updated_at = timeStamp.toISOString();
+
+  pool.query(
+    'UPDATE inventory SET item_name = $1, description = $2, price = $3, quantity = $4, provider = $5, updated_at = $6 WHERE id = $7',
+    [
+      updatedData.item_name,
+      updatedData.description,
+      updatedData.price,
+      updatedData.quantity,
+      updatedData.provider,
+      updated_at,
+      updatedData.id,
+    ],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      console.log(`Se actualizó el registro con ID ${updatedData.id}`);
+    },
+  );
+  pool.end();
+});
+
 router.post('/new', function (req, res, next) {
   //get all inventorys from DB
-  const { id, item_name, description, quantity, price, created_by } = req.body;
+  const { id, item_name, description, quantity, price, provider } = req.body;
   let response;
   const timeStamp = new Date();
   const created_at = timeStamp.toISOString();
   const updated_at = null;
+  const created_by = 1;
 
   const pool = new Pool({
     user: 'sergio-martinez',
@@ -49,9 +85,8 @@ router.post('/new', function (req, res, next) {
     password: '',
     port: 5432, // puerto predeterminado de PostgreSQL
   });
-
   pool.query(
-    'INSERT INTO inventory(id,item_name,description,quantity,price,created_by,created_at,updated_at,) VALUES ($1, $2, $3,$4,$5,$6, $7, $8)',
+    'INSERT INTO inventory(id,item_name,description,quantity,price,created_by,created_at,updated_at, provider) VALUES ($1, $2, $3,$4,$5,$6, $7, $8, $9)',
     [
       id,
       item_name,
@@ -61,6 +96,7 @@ router.post('/new', function (req, res, next) {
       created_by,
       created_at,
       updated_at,
+      provider,
     ],
     (error, results) => {
       if (error) {
@@ -81,7 +117,7 @@ router.post('/new', function (req, res, next) {
   pool.end();
 });
 
-router.delete('/:id', function (req, res, next) {
+router.delete('/delete/:id', function (req, res, next) {
   const pool = new Pool({
     user: 'sergio-martinez',
     host: 'localhost',
@@ -89,12 +125,14 @@ router.delete('/:id', function (req, res, next) {
     password: '',
     port: 5432, // puerto predeterminado de PostgreSQL
   });
+  const { id } = req.params;
+  
 
-  pool.query('DELETE FROM inventory WHERE name = $1', [id], (err, res) => {
+  pool.query('DELETE FROM inventory WHERE id = $1', [id], (err, res) => {
     if (err) {
-      console.error('Error al eliminar usuario:', err.stack);
+      console.error('Error al eliminar producto:', err.stack);
     } else {
-      console.log(`Usuario con NAME ${id} eliminado con éxito`);
+      console.log(`Producto ${id} eliminado con éxito`);
     }
     pool.end();
   });
